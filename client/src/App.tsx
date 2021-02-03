7b0dac6f3c179906abaabbcef4112232d45939ca
sochacki111 <michalsochacki111@gmail.com>
2021-02-03 20:01:23 +0100

    Add basic login and register screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
+import RegisterScreen from './components/RegisterScreen';
+import SigninScreen from './components/SigninScreen';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world</header>
+      <main>
+        <Route path="/signin" component={SigninScreen}></Route>
+        <Route path="/register" component={RegisterScreen}></Route>
+      </main>
     </div>
   );
 }
